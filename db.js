5192aebb52d312086e9648f95a47473682e78890
Kamil <kamil.dmytrenko@gmail.com>
2018-07-01 19:19:03 +0200

    add db config

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+let port = process.env.PORT;
+
+module.exports = (app) => {
+  // Connect to database
+  mongoose.connect(process.env.DB_HOST)
+    .then(() => {
+      //Start app only after connection is ready
+      app.listen(port, () => {
+        console.log(`Your application is running on http://localhost:${port}`);
+      });
+    })
+};
